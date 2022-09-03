4db7725dabfd422c4a7c4b96d111d64ab17ab8c9
Anubhav Kumar <anubhav.p.kumar@gmail.com>
2022-09-03 14:30:00 +0530

    Sample Stateful component tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Heading from './components/heading';
+import StatefulComponent from './components/stateful-component';
 
 function App() {
   return (
     <div className="App">
       <Heading heading='Anubhav Kumar' subHeading='Senior Software Developer, Dreamsetgo' />
+      <StatefulComponent initialNumber={10} />
+      <StatefulComponent initialNumber={0} />
     </div>
   );
 }
