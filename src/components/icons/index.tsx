0aacfab2f56c554e97ed0b446ba99ce2131f151b
Anubhav Kumar <anubhav.p.kumar@gmail.com>
2022-09-04 20:06:33 +0530

    Icons added

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,6 +1,10 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { iconProps } from '../../data-types';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const Icon = (props: iconProps) => {
     const {name} = props;
@@ -9,6 +13,14 @@ const Icon = (props: iconProps) => {
             return <KeyboardArrowDownIcon />
         case 'upArrow': 
             return <KeyboardArrowUpIcon />
+        case 'delete': 
+            return <DeleteIcon color={'error'} />
+        case 'edit': 
+            return <EditIcon color={'primary'}/>
+        case 'save': 
+            return <CheckCircleIcon color={'success'} />
+        case 'cancel': 
+            return <CancelIcon color={'error'} />
         default:
             return null;
     }
